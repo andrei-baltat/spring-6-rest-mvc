78bfc1a1e1ebfc6e8cfb7164c0a298a20054a3a7
andrei.baltat <andrei.baltat@adswizz.com>
2023-03-08 16:36:48 +0200

    testListBeers unit test

--- src/test/java/com/example/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/example/spring6restmvc/controller/BeerControllerTest.java
@@ -13,12 +13,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.core.Is.is;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
@@ -51,7 +54,48 @@ class BeerControllerTest {
 
         mockMvc.perform(get("/api/v1/beer/" + id).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(galaxyCat.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(galaxyCat.getBeerName())));
+    }
 
+
+    @Test
+    @SneakyThrows
+    void testListBeers(){
+        UUID id = UUID.randomUUID();
+
+        Beer galaxyCat1 = Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("1234")
+                .quantityOnHand(900)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .price(new BigDecimal("12.99"))
+                .build();
+
+        Beer galaxyCat2 = Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy cat1")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("1234")
+                .quantityOnHand(900)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .price(new BigDecimal("12.99"))
+                .build();
+
+        when(beerService.listBeers()).thenReturn(List.of(
+                galaxyCat1, galaxyCat2
+        ));
+
+        mockMvc.perform(get("/api/v1/beer" + id).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(2)));
     }
 }
