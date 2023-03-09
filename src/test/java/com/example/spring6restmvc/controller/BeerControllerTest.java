60e280525d6f989990fb5eec3e9edbd58da49ffe
andrei.baltat <andrei.baltat@adswizz.com>
2023-03-09 07:46:30 +0200

    +  patch and delete unit test + argument captor

--- src/test/java/com/example/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/example/spring6restmvc/controller/BeerControllerTest.java
@@ -6,6 +6,8 @@ import com.example.spring6restmvc.services.BeerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,14 +16,18 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
@@ -42,6 +49,74 @@ class BeerControllerTest {
 
     @MockBean // put in context a beer service mock bean
     BeerService beerService;
+
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<Beer> beerArgumentCaptor;
+
+    @Test
+    @SneakyThrows
+    void testPatchBeer() {
+        UUID id = UUID.randomUUID();
+
+        Beer beer = Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("1234")
+                .quantityOnHand(900)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .price(new BigDecimal("12.99"))
+                .build();
+
+        Map<String, Object> beerMap = new HashMap<>();
+        beerMap.put("beerName", "New name");
+
+        mockMvc.perform(
+                        patch(URL + "/" + id)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(mapper.writeValueAsString(beer)))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).patchBeerById(uuidArgumentCaptor.capture(), beerArgumentCaptor.capture());
+        assertThat(id).isEqualTo(uuidArgumentCaptor.getValue());
+        assertThat(beerMap.get("beerName")).isEqualTo(beerArgumentCaptor.getValue());
+
+    }
+
+    @Test
+    @SneakyThrows
+    void deleteBeer() {
+        UUID id = UUID.randomUUID();
+
+        Beer beer = Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("1234")
+                .quantityOnHand(900)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .price(new BigDecimal("12.99"))
+                .build();
+
+        mockMvc.perform(delete(URL + "/" + beer.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        // listens for an argument that is passed in
+
+        verify(beerService).deleteById(uuidArgumentCaptor.capture());
+
+        assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+
+    }
 
     @Test
     @SneakyThrows
@@ -67,14 +142,14 @@ class BeerControllerTest {
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(galaxyCat)))
-                        .andExpect(status().isCreated())
-                                .andExpect(header().exists("Location"));
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
 
     }
 
     @Test
     @SneakyThrows
-    void testForUpdateBeer(){
+    void testForUpdateBeer() {
         UUID id = UUID.randomUUID();
 
         Beer beer = Beer.builder()
@@ -91,9 +166,9 @@ class BeerControllerTest {
 
 
         mockMvc.perform(put(URL + "/" + beer.getId())
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(beer)))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(beer)))
                 .andExpect(status().isNoContent());
 
         verify(beerService).updateBeerById(any(UUID.class), any(Beer.class)); // will verify one interaction
